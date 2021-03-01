cae6a230f48ff9f3120e6c45ed2cb2027125e618
Kusn <serjikf@yandex.ru>
2021-03-01 16:36:40 +0300

    Создал формы и обработчик нажатия кнопки

---
+++ Home_work1/WinFormsTestMailSender/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace WinFormsTestMailSender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSendMail = new System.Windows.Forms.Button();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSendMail
+            // 
+            this.btnSendMail.Location = new System.Drawing.Point(690, 415);
+            this.btnSendMail.Name = "btnSendMail";
+            this.btnSendMail.Size = new System.Drawing.Size(98, 23);
+            this.btnSendMail.TabIndex = 0;
+            this.btnSendMail.Text = "Отправить email";
+            this.btnSendMail.UseVisualStyleBackColor = true;
+            this.btnSendMail.Click += new System.EventHandler(this.btnSendMail_Click);
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(13, 243);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.PasswordChar = '*';
+            this.passwordBox.Size = new System.Drawing.Size(193, 20);
+            this.passwordBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.btnSendMail);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSendMail;
+        private System.Windows.Forms.TextBox passwordBox;
+    }
+}
+
---
+++ Home_work1/WpfTestMailSender/WpfMailSender.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Mail;
+
+namespace WpfTestMailSender
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class WpfMailSender : Window
+    {
+        public WpfMailSender()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSendEmail_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> listStrMails = new List<string> { "testEmail@gmail.com", "email@yandex.ru" };  // Список email'ов //кому мы отправляем письмо
+            string strPassword = passwordBox.Password;  // для WinForms - string strPassword = passwordBox.Text;
+            foreach (string mail in listStrMails)
+            {
+                // Используем using, чтобы гарантированно удалить объект MailMessage после использования
+                using (MailMessage mm = new MailMessage("sender@yandex.ru", mail))
+                {
+                    // Формируем письмо
+                    mm.Subject = "Привет из C#"; // Заголовок письма
+                    mm.Body = "Hello, world!";       // Тело письма
+                    mm.IsBodyHtml = false;           // Не используем html в теле письма
+                                                     // Авторизуемся на smtp-сервере и отправляем письмо
+                                                     // Оператор using гарантирует вызов метода Dispose, даже если при вызове 
+                                                     // методов в объекте происходит исключение.
+                    using (SmtpClient sc = new SmtpClient("smtp.yandex.ru", 25))
+                    {
+                        sc.EnableSsl = true;
+                        sc.Credentials = new NetworkCredential("sender@yandex.ru", strPassword);
+                        try
+                        {
+                            sc.Send(mm);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Невозможно отправить письмо " + ex.ToString());
+                        }
+                    }
+                } //using (MailMessage mm = new MailMessage("sender@yandex.ru", mail))
+            }
+            MessageBox.Show("Работа завершена.");
+        }
+    }
+}
