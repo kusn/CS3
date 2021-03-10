68f30f916a52f6fb20ebe00c9ceb3cd734b62df1
kusn <serjikf@yandex.ru>
2021-03-10 07:55:30 +0300

    Начал делать в MVVM

---
+++ Home_work2/MailerMVVM/View/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MailerMVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TabSwitcher_btnPrevClick(object sender, RoutedEventArgs e)
+        {
+            if (tabControl.SelectedIndex == 0) tabControl.SelectedIndex = tabControl.Items.Count - 1;
+            else tabControl.SelectedIndex--;
+        }
+
+        private void btPlanner_Click(object sender, RoutedEventArgs e)
+        {
+            tabControl.SelectedItem = tabPlanner;
+        }
+
+        private void TabSwitcher_btnNextClick(object sender, RoutedEventArgs e)
+        {
+            if (tabControl.SelectedIndex == tabControl.Items.Count - 1) tabControl.SelectedIndex = 0;
+            else tabControl.SelectedIndex++;
+        }
+    }
+}
