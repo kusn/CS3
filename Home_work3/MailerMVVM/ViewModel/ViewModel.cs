5b0f0368e84f619cfe80eb940888e91ec64f860a
Kusn <serjikf@yandex.ru>
2021-03-15 19:35:30 +0300

    Что-то не особо рабочее в MailerMVVM

--- Home_work3/MailerMVVM/ViewModel/ViewModel.cs
+++ Home_work3/MailerMVVM/ViewModel/ViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using System.Windows.Input;
 
 namespace MailerMVVM.ViewModel
 {
@@ -11,6 +12,38 @@ namespace MailerMVVM.ViewModel
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
+        public ICommand TSBtnPrevClic
+        {
+            get
+            {
+                return new DelegateCommand(TSBtnPrevExecute, TSBtnPrevCanExecute);
+            }
+        }
 
+        private void TSBtnPrevExecute(object obj)
+        {
+        }
+
+        private bool TSBtnPrevCanExecute(object obj)
+        {
+            return true;
+        }
+
+        public ICommand MIExitClick
+        {
+            get
+            {
+                return new DelegateCommand(MIExitExecute, MIExitCanExecute);
+            }
+        }
+
+        private void MIExitExecute(object obj)
+        {
+        }
+
+        private bool MIExitCanExecute(object obj)
+        {
+            return true;
+        }
     }
 }
