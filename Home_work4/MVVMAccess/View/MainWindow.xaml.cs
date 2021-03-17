d40e53442fd2ec6b4c767dc8d508fdf8d51fe482
kusn <serjikf@yandex.ru>
2021-03-17 07:32:16 +0300

    Начал делать ДЗ 4

---
+++ Home_work4/MVVMAccess/View/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿// Кудряшов Сергей
+
+// 1.Доработать приложение MVVMAccess.
+// а.Добавить свойство Access информирующее, получил ли пользователь доступ.
+// б.Добавить команду Clear - которое очищает поля логина и пароля.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVVMAccess
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Home_work4/MailerMVVM/ViewModel/ViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace MailerMVVM.ViewModel
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand TSBtnPrevClic
+        {
+            get
+            {
+                return new DelegateCommand(TSBtnPrevExecute, TSBtnPrevCanExecute);
+            }
+        }
+
+        private void TSBtnPrevExecute(object obj)
+        {
+        }
+
+        private bool TSBtnPrevCanExecute(object obj)
+        {
+            return true;
+        }
+
+        public ICommand MIExitClick
+        {
+            get
+            {
+                return new DelegateCommand(MIExitExecute, MIExitCanExecute);
+            }
+        }
+
+        private void MIExitExecute(object obj)
+        {
+        }
+
+        private bool MIExitCanExecute(object obj)
+        {
+            return true;
+        }
+    }
+}
