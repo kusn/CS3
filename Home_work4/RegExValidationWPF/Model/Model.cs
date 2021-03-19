98b5d6b1723f95ecb8f2ca3a1f9dbec06e32fca2
kusn <serjikf@yandex.ru>
2021-03-19 17:39:31 +0300

    Доделал ДЗ

--- Home_work4/RegExValidationWPF/Model/Model.cs
+++ Home_work4/RegExValidationWPF/Model/Model.cs
@@ -82,5 +82,13 @@ namespace RegExValidationWPF.Model
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public static int Min(int a, int b, int c)
+        {
+            int min = a;
+            if (min > b) min = b;
+            if (min > c) min = c;
+            return min;
+        }
     }
 }
---
+++ Home_work4/RegExValidationWPFTests/Model/AccountTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegExValidationWPF.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegExValidationWPF.Model.Tests
+{
+    [TestClass()]
+    public class AccountTests
+    {
+        [TestMethod()]
+        public void MinTest()
+        {
+            int min;
+            min = Account.Min(3, 4, 5);
+            Assert.AreEqual(3, min);
+        }
+    }
+}
