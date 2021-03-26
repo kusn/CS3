4f0e373ccbc8f75a74fb37a407c612b95137f75f
Kusn <serjikf@yandex.ru>
2021-03-26 22:57:03 +0300

    Начал выполнение ДЗ 7

---
+++ Home_work7/MovieTicketSalesEF/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+using Microsoft.Reporting.WinForms;
+using System.Data.Entity.Core.Objects;
+
+namespace MovieTicketSalesEF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MovieDBContainer _dbContainer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = _dbContainer;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _dbContainer = new MovieDBContainer();
+            _dbContainer.Seances.Load();
+            Grid.ItemsSource = _dbContainer.Seances.Local;
+        }
+
+        private void btnAddSeance_Click(object sender, RoutedEventArgs e)
+        {
+            WndAddSeance wndAddSeance = new WndAddSeance();
+            //wndAddSeance.Show();
+
+            if (wndAddSeance.ShowDialog() == true)
+            {
+                _dbContainer.Seances.Load();
+                Grid.Items.Refresh();
+            }
+        }
+
+        private void cmmiBuy_Click(object sender, RoutedEventArgs e)
+        {
+            WndBuy wndBuy = new WndBuy();
+            wndBuy.Show();
+            //wndBuy.ShowDialog();
+        }
+    }
+}
---
+++ Home_work7/MovieTicketSalesEF/MovieDB.edmx.sql
@@ -0,0 +1,84 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 03/26/2021 18:12:52
+-- Generated from EDMX file: D:\Учеба\CS3\Home_work7\MovieTicketSalesEF\MovieDB.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [MovieDB];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Orders'
+CREATE TABLE [dbo].[Orders] (
+    [OrderId] int IDENTITY(1,1) NOT NULL,
+    [TiketsCount] int  NOT NULL,
+    [OrderTime] time  NOT NULL,
+    [SeanceSeanceId] int  NOT NULL
+);
+GO
+
+-- Creating table 'Seances'
+CREATE TABLE [dbo].[Seances] (
+    [SeanceId] int IDENTITY(1,1) NOT NULL,
+    [MovieName] nvarchar(max)  NOT NULL,
+    [SeanceTime] time  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [OrderId] in table 'Orders'
+ALTER TABLE [dbo].[Orders]
+ADD CONSTRAINT [PK_Orders]
+    PRIMARY KEY CLUSTERED ([OrderId] ASC);
+GO
+
+-- Creating primary key on [SeanceId] in table 'Seances'
+ALTER TABLE [dbo].[Seances]
+ADD CONSTRAINT [PK_Seances]
+    PRIMARY KEY CLUSTERED ([SeanceId] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [SeanceSeanceId] in table 'Orders'
+ALTER TABLE [dbo].[Orders]
+ADD CONSTRAINT [FK_OrderSeance]
+    FOREIGN KEY ([SeanceSeanceId])
+    REFERENCES [dbo].[Seances]
+        ([SeanceId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_OrderSeance'
+CREATE INDEX [IX_FK_OrderSeance]
+ON [dbo].[Orders]
+    ([SeanceSeanceId]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
