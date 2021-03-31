1f610f98148bc6ebbd506508f0727e444b654604
kusn <serjikf@yandex.ru>
2021-03-31 08:28:53 +0300

    Начал выполнение ДЗ 8

---
+++ Home_work8/Checks/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checks
+{
+    class Program
+    {
+        private enum SomeEnum
+        {
+            First = 4,
+            Second,
+            Third,
+            Fourth = 7
+        }
+
+        static void Main(string[] args)
+        {
+            // 2. Есть ли проблемы в следующем коде?
+            /*int i = 1;
+            object obj = i;
+            ++i;
+            Console.WriteLine(i);
+            Console.WriteLine(obj);
+            Console.WriteLine((short)obj);*/
+
+            // 4. Каков результат вывода следующего кода?
+            Console.WriteLine((int)SomeEnum.Second);
+            Console.WriteLine((int)SomeEnum.Third);
+
+            Console.ReadLine();
+        }
+    }
+}
